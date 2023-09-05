7c8b0970e56219780564bc122a97e05ba359603b
reo113 <roberteliortiz@gmail.com>
2023-09-05 15:43:03 -0400

    after holiday break did an additon hard ques

---
+++ medianofTwosortedArrays/findmedianSortedArrays.js
@@ -0,0 +1,12 @@
+const findMedianSortedArrays = (arr1, arr2) => {
+  const newarr = arr1.concat(arr2).sort((a, b) => a - b);
+  console.log(newarr);
+  let left = 0;
+  let right = newarr.length - 1;
+  while (left < right) {
+    left++;
+    right--;
+  }
+  return (newarr[left] + newarr[right]) / 2;
+};
+console.log(findMedianSortedArrays([3], [-2, -1]));
