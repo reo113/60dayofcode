88211e48ee4d75c2914dedebb1135133c13625e5
reo113 <roberteliortiz@gmail.com>
2023-09-14 15:39:33 -0400

    finished the day 13 solution

---
+++ #13 balancedBinaryTree/balancedBT.js
@@ -0,0 +1,13 @@
+const balancedBinarytree = (root) => {
+  function getheight(root) {
+    if (root == null) return 0;
+
+    const left = getheight(root.left);
+    const right = getheight(root.right);
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) return -1;
+    return Math.max(left, right) + 1;
+  }
+  if (root === null) return true;
+  if (getheight(root) === -1) return false;
+  return true;
+};
